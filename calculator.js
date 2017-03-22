6a6e7b48899fa9ab040ea392078b24b44a4d4a98
Teresa <trothaar@users.noreply.github.com>
2017-03-21 21:08:55 -0400

    Add files via upload

--- calculator.js
+++ calculator.js
@@ -3,16 +3,17 @@ $(document).ready(function(){
   var inputs=[""];
   //String to store current input string
   var totalString;
- //Operators array for validation without .
+ //Operators array for validation without decimal
   var operators1 = ["+","-","/","*"];
  //operators array for validation with decimal
   var operators2 = ["."];
   //Numbers array for validation
   var nums=[0,1,2,3,4,5,6,7,8,9];
+
   //Adds value to inputs array and checks validation
   function getValue(input){
      if(operators2.includes(inputs[inputs.length-1])===true && input==="."){
-    console.log("Duplicate '.'");
+     console.log("Duplicate '.'");
   }
    // Make sure expression does not start with an operator
     else if(inputs.length===1 && operators1.includes(input)===false){
@@ -27,25 +28,30 @@ $(document).ready(function(){
     }
     update();
   }
+
+// Updates inputs array accordingly and displays value in entry box
   function update(){
     totalString= inputs.join("");
     $("#steps").html(totalString);
     console.log(inputs);
   }
+
+// Totals everything up and displays it in entry box
   function getTotal(){
     totalString = inputs.join("");
     $("#steps").html(eval(totalString));
   }
 
+// Clears inputs array if user clicks deleteAll
   $("a").on("click",function(){
     if(this.id==="deleteAll"){
       inputs = [""];
       update();
-    }
+    } // Clears last entry if user clicks backOne
     else if(this.id==="backOne"){
       inputs.pop();
       update();
-    }
+    } // Otherwise, total everything up
     else if(this.id==="total"){
       getTotal();
     }
